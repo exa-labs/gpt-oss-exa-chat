502e2ea83cef52f41044902284ea30db2d05e704
Ishan Goswami <ishangoswami315@gmail.com>
2025-08-07 04:05:16 +0530

    added together ai

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,4 +1,4 @@
-import { openai } from '@ai-sdk/openai';
+import { togetherai } from '@ai-sdk/togetherai';
 import { streamText, tool } from 'ai';
 import { z } from 'zod';
 import Exa from 'exa-js';
@@ -12,13 +12,11 @@ export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const result = streamText({
-    model: openai('o3-mini'),
+    model: togetherai('openai/gpt-oss-120b') as any,
     system: 'Always use the webSearch tool. Always provide source links in your response (the sources which you got from the webSearch tool). You are a helpful assistant that searches the web for information and provides accurate answer. Use simple english. Use the webSearch tool in every message!',
     messages,
-    providerOptions: {
-        openai: { reasoningEffort: 'medium' },
-      },
     maxSteps: 2,
+    experimental_toolCallStreaming: true,
     tools: {
       webSearch: tool({
         description: 'Search the web for current information on a topic. Use this tool in every message, always. Always provide source links in your response.',
@@ -28,11 +26,8 @@ export async function POST(req: Request) {
         execute: async ({ query }) => {
           try {
             const results = await exa.search(query, {
-              
-              
               numResults: 5,
               type: 'auto',
-              useAutoprompt: true
             });
             return results.results;
           } catch (error) {
